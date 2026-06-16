3ee4804d0c0627f122fcd9881a760c62b4000434
ssxshi <prod.sushi6@gmail.com>
2026-06-15 22:34:14 -0400

    svr cli added

--- main.cpp
+++ main.cpp
@@ -2,12 +2,37 @@
 #include <stdio.h>
 
 #include "json.hpp"
-#include "http.h"
+#include "httplib.h"
 
-using json = nlohmann::json;
+using namespace nlohmann;
+using namespace std;
+using namespace httplib;
+
+Server svr;
+Client cli("https://discord.com");
+
+bool to_bot(json &data){
+    string payload = data.dump();
+
+    auto result = cli.Post(getenv("DISCORD_WH"), payload, "application/json");
+
+    if (result && result->status == 200){
+        return true;
+    }
+
+    return false;
+}
+
+void handle_alert(const Request &req, Response &res){
+    json body = json::parse(req.body);
+    if (body.contains("is_test") && body["is_test"] == true){
+        res.set_content("works", "text/plain");
+    }
+}
 
 int main(){
-    
+    svr.Post("/alert", handle_alert);
+    svr.listen("0.0.0.0", 8080);
 
     exit(0);
 }
